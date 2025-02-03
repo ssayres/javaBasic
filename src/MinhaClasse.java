31cc785255aee22cad9a7e8c9a5dc58350cd08da
Sidnei Oliveira | Tecnologia | Via Expressa <sidnei.oliveira@viaexpressa.com>
2025-02-03 12:23:50 -0300

    Anatomia das Classes-3

---
+++ src/MinhaClasse.java
@@ -0,0 +1,26 @@
+
+
+public class MinhaClasse {
+
+    public static void main (String [] args) {
+        //System.out.println("Olá mundo!");
+
+        // final String BR = "Brasil"; constante por convenção vem como UPPERCASE
+        // jamais iniciar variavel com numero
+        // variavel camel case - minhaVariavel
+        // padrao de arquivos e classes- MinhaClasse
+        // $ é permitido - variavel$um
+
+        // Tipo NomeBemDefinido = Atribuição (opcional)
+
+        String nome = "Glauber";
+        String sobreNome = "Carvalho";
+
+        String nomeCompleto = nomeCompleto(nome, sobreNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String nome, String sobreNome){
+        return nome.concat(" ").concat(sobreNome);
+    }
+}
