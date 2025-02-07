a46328ba47c36a272e544cb54f02248ee975bd15
Sidnei Oliveira | Tecnologia | Via Expressa <sidnei.oliveira@viaexpressa.com>
2025-02-07 11:44:10 -0300

    Tipos e Variáveis - 4

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception{
+        double salarioMinimo = 2000;
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal; // casting
+
+        String meuNome = "Sidnei Ayres";
+
+    }
+}
