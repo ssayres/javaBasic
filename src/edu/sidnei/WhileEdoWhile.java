ad39fd7b006d574a36c7929d3c4d1923f70bcf46
ssayres <ssayres@outlook.com>
2025-03-07 22:15:11 -0300

    praticando laço while e praticando debugador também

---
+++ src/edu/sidnei/WhileEdoWhile.java
@@ -0,0 +1,21 @@
+package edu.sidnei;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class WhileEdoWhile {
+    public static void main(String[] args) {
+        double saldo = 50;
+
+        while (saldo>0){
+            Double valorDoce = valorAleatorio();
+            if(valorDoce>saldo)
+                valorDoce = saldo;
+            System.out.println("Doce valor " + valorDoce + " Adicionado ao carrinho"  );
+            saldo = saldo - valorDoce;
+        }
+        System.out.println("Saldo: " + saldo);
+    }
+
+    private  static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2,8);
+    }
+}
